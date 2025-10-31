c9dab015e4a921a5b1b97f0073b26b8409729f70
FacundoNicolasTorres <facujaritor@gmail.com>
2025-10-31 17:00:08 -0300

    primer parte de crearPost

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,7 +1,34 @@
-
-
+import { CircleFadingPlus } from "lucide-react";
+import style from "./CreatePost.module.css"
 export default function CreatePost() {
   return (
-    <div>CreatePost</div>
-  )
+    <div className="text-light">
+      <h3>Crear Nueva Publicación</h3>
+      <p className="text-secondary">Comparte tu contenido con la comunidad</p>
+
+      <div className={`card mb-3 ${style.cardPost}`}> {/*Div crear Post */}
+        <div className="card-body">
+        <label htmlFor="description" className="form-label">
+          Descripción *
+        </label>
+        <textarea
+          id="description"
+          className={`form-control text-light border-secondary ${style.formControl}`}
+          placeholder="¿Qué quieres compartir?"
+          rows={4}
+          style={{ resize: "none" }}
+        />
+        </div>
+      </div>
+
+      <div className="d-flex gap-3"> {/*Div botones */}
+        <button type="button" className="btn btn-success d-flex align-items-center gap-2">
+          <CircleFadingPlus /> Publicar
+        </button>
+        <button type="button" className="btn btn-secondary">
+          Cancelar
+        </button>
+      </div>
+    </div>
+  );
 }
