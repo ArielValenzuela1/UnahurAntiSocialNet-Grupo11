79a48d27216e191337f21f02776f23eae7b70d5e
FacundoNicolasTorres <facujaritor@gmail.com>
2025-10-31 12:59:53 -0300

    Logo de la red social modificado

--- src/components/LogoInicio.tsx
+++ src/components/LogoInicio.tsx
@@ -1,10 +1,15 @@
 import FondoUnahur from "./FondoUnahur";
-
+import logoUnaHur from "../assets/img/logo-UnaHur.png";
+import style from "./LogoInicio.module.css";
 export default function LogoInicio() {
   return (
     <div style={{ display: "inline-flex" }}>
-        <FondoUnahur/>
-        <p>UNAHUR Anti-Social</p>
+      <div className={`${style.divLogo}`}>
+        <FondoUnahur>
+          <img className={`${style.logo}`} src={logoUnaHur} />
+        </FondoUnahur>
+      </div>
+        <p className={`${style.redSocial}`}>UnaHur Anti-Social</p>
     </div>
   )
 }
