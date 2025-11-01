619f8a8fd8eeac7125ede7a3aa0500e0cf906e2b
Franco <francoiannone2@gmail.com>
2025-10-31 23:30:18 -0300

    perfil parte 1

--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -1,6 +1,40 @@
+import CirculoLetra from "../components/CirculoLetra";
 
 export default function Perfil() {
   return (
-    <div>Perfil</div>
+    <>
+      <div style={{display: "flex", alignItems: "center"}}>
+        <CirculoLetra letra='L' size={100} />
+        <div>
+          <h3>nombre Usuario</h3>
+          <div style={{display: "flex"}}>
+            <div>
+              <p>publicaciones</p>
+              <p>cantidad</p>
+            </div>
+            <div>
+              <p>Seguidores</p>
+              <p>cantidad</p>
+            </div>
+            <div>
+              <p>Siguiendo</p>
+              <p>cantidad</p>
+            </div>
+            <div>
+              <p>Miembro Desde</p>
+              <p>fecha</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1>Mis Publicaciones</h1>
+        <div>
+          {/* icono */}
+          <p>Aún no has creado ninguna publicación</p>
+          <button></button>
+        </div>
+      </div>
+    </>
   )
 }
