9a9c7008eef9e00865402ab889f025f5c1214eb6
FacundoNicolasTorres <facujaritor@gmail.com>
2025-11-01 10:49:26 -0300

    Agregar varias Imagenes al crear un post

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,6 +1,24 @@
-import { CircleFadingPlus } from "lucide-react";
-import style from "./CreatePost.module.css"
+import { CircleFadingPlus, X, Image } from "lucide-react";
+import style from "./CreatePost.module.css";
+import { useState } from "react";
+
 export default function CreatePost() {
+  const [imagenes, setImagenes] = useState([""]);
+
+  const cambiarValor = (index:number, value:string) => {
+    const nuevasImg = [...imagenes];
+    nuevasImg[index] = value;
+    setImagenes(nuevasImg);
+  };
+
+  const eliminarImagen = (index:number) => {
+    setImagenes((previo) => previo.filter((_, i) => i !== index));
+  };
+
+  const agregarImagen = () => {
+    setImagenes((prev) => [...prev, ""]);
+  };
+
   return (
     <div className="text-light">
       <h3>Crear Nueva Publicación</h3>
@@ -8,16 +26,40 @@ export default function CreatePost() {
 
       <div className={`card mb-3 ${style.cardPost}`}> {/*Div crear Post */}
         <div className="card-body">
-        <label htmlFor="description" className="form-label">
-          Descripción *
-        </label>
-        <textarea
-          id="description"
-          className={`form-control text-light border-secondary ${style.formControl}`}
-          placeholder="¿Qué quieres compartir?"
-          rows={4}
-          style={{ resize: "none" }}
-        />
+          <p>Contenido</p>
+          <label htmlFor="description" className="form-label">Descripción *</label>
+          <textarea
+            id="description"
+            className={`form-control text-light border-secondary ${style.formControl}`}
+            placeholder="¿Qué quieres compartir?"
+            rows={4} />
+
+          {/*Crear imagenes */}
+          <div className={`${style.divSeccionImagen}`}>
+            <label htmlFor="Imágenes" className="form-label">Imágenes (opcional)</label>
+            {imagenes.map((url, index) => (
+              <div className={`input-group ${style.divCrearImagen}`}>
+
+                <input type="text" 
+                className={`form-control border-secondary ${style.inputUrlImagen}`} 
+                placeholder="https://ejemplo.com/imagen.jpg" 
+                aria-describedby="button-addon2"
+                value={url}
+                onChange={(e) => cambiarValor(index, e.target.value)} />
+
+                <button className={`btn ${imagenes.length > 1 ? style.btnBorrarImagenAct : style.btnBorrarImagenDes}`} 
+                type="button" 
+                id="button-addon2"
+                onClick={() => eliminarImagen(index)}> <X /> </button>
+              </div>
+            ))}
+            <button type="button" 
+            className={`btn btn-secondary ${style.botonAgregarImagen}`}
+            onClick={agregarImagen}> <Image /> Agregar otra imagen</button>
+
+          </div>
+
+
         </div>
       </div>
 
@@ -31,4 +73,4 @@ export default function CreatePost() {
       </div>
     </div>
   );
-}
+};
