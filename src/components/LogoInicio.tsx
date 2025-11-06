49048189e5b7a6b9924e66e91b268208ea4a76b4
FacundoNicolasTorres <facujaritor@gmail.com>
2025-11-06 01:34:55 -0300

    ruta a home en logoInicio

--- src/components/LogoInicio.tsx
+++ src/components/LogoInicio.tsx
@@ -2,6 +2,7 @@ import FondoUnahur from "./FondoUnahur";
 import logoUnaHur from "../assets/img/logo-UnaHur.png";
 import style from "./Modules/LogoInicio.module.css";
 export default function LogoInicio() {
+
   return (
     <div className={`d-inline-flex ${style.contenedorLogo}`}>
       <div className={`${style.divLogo}`}>
@@ -9,7 +10,7 @@ export default function LogoInicio() {
           <img className={`${style.logo}`} src={logoUnaHur} />
         </FondoUnahur>
       </div>
-        <p className={`${style.redSocial}`}>UnaHur Anti-Social</p>
+      <p className={`${style.redSocial}`}>UnaHur Anti-Social</p>
     </div>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,24 @@
 import LogoInicio from "./LogoInicio";
 import GestorDeSesion from "./GestorDeSesion";
 import style from "./Modules/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
+  const handleInicioClick = () => {
+    if (location.pathname === "/")
+      window.location.reload();
+  };
   return (
     <div className= {`${style.NavBar} sticky-top`}>
-        <LogoInicio/>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `${style.NavLink} ${isActive ? style.activo : ""}`
+          }
+          onClick={handleInicioClick}
+        >
+          <LogoInicio/>
+        </NavLink>
         <GestorDeSesion />
     </div>
   )
